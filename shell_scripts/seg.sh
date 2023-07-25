feff06a7041d930367bd891d9bee68d113d161b1
Vily1998 <2512857469@qq.com>
2023-07-25 11:21:59 +0000

    first commit

--- shell_scripts/seg.sh
+++ shell_scripts/seg.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=0
 MUSTC_ROOT=path_to_mustc_data
 LANG=de
 SAVE_DIR=path_to_save_checkpoints
-OUPUT_SEG=path_to_save_segment
+OUTPUT_SEG=path_to_save_segment
 
 WAV=path_to_wav_file
 
@@ -11,5 +11,5 @@ python segment.py ${MUSTC_ROOT}/en-${LANG} \
     --task speech_to_text_multitask  \
     --config-yaml config_raw.yaml \
     --ckpt ${SAVE_DIR}/average-model.pt \
-    --save-root ${OUPUT_SEG} \
-    --wav ${wav}
+    --save-root ${OUTPUT_SEG} \
+    --wav ${WAV}
